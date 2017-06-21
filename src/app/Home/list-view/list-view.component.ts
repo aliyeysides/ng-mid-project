97011dc489e9c0eacac5678da54f368088513337
ali yeysides <ali.yeysides@gmail.com>
2017-06-21 15:58:21 -0400

    adds ngfor for list in markup and fake data

--- src/app/Home/list-view/list-view.component.ts
+++ src/app/Home/list-view/list-view.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListViewComponent implements OnInit {
 
+  public list: Array<object>;
+
   constructor() { }
 
   ngOnInit() {
+    this.list = [
+      {rating: 'bullish', ticker: 'CSMA', name: 'Comcast', price: '$42.50', change: '5%'},
+      {rating: 'very bullish', ticker: 'AMGN', name: 'Amgen', price: '$112.50', change: '5%'},
+      {rating: 'bearish', ticker: 'AMD', name: 'Advanced Micro Something', price: '$12.50', change: '5%'},
+      {rating: 'very bearish', ticker: 'RRC', name: 'Robin Jobin Oil', price: '$9.50', change: '5%'},
+      {rating: 'neutral', ticker: 'ULTA', name: 'Ulta', price: '$312.50', change: '5%'}
+    ]
   }
 
 }

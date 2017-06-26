c05b332bc19f54cec736c8ae21506b5e4af5f81a
ali yeysides <ali.yeysides@gmail.com>
2017-06-26 10:13:21 -0400

    fixes form module

--- src/app/shared/symbol-search/symbol-search.module.ts
+++ src/app/shared/symbol-search/symbol-search.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {SymbolSearchComponent} from './symbol-search.component';
 import {SearchPanelComponent} from '../search-panel/search-panel.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     SymbolSearchComponent,

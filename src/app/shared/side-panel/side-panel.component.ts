3f967bf09a7282d9daa46c7a45e3502599d05f5d
hardeep.deol@paxcel.net <hardeep.deol@paxcel.net>
2017-07-18 12:01:41 +0530

    ädd moment module for date format

--- src/app/shared/side-panel/side-panel.component.ts
+++ src/app/shared/side-panel/side-panel.component.ts
@@ -5,7 +5,8 @@ import { SidePanelProvider } from 'app/providers/side-panel.provider';
 import { PagerProvider } from 'app/providers/paging.provider';
 import { IdeaListProvider } from 'app/providers/idea-list.provider';
 // import * as d3 from 'd3';
-import * as moment from 'moment/moment';
+//import * as moment from 'moment/moment';
+import * as moment from 'moment-timezone'
 
 @Component({
   selector: 'side-panel',
@@ -59,14 +60,16 @@ export class SidePanelComponent implements OnInit {
 
 	public symbol: string = 'SPY';
 	constructor(private sidePanelProvider: SidePanelProvider, private ideaListProvider: IdeaListProvider, private pagerProvider: PagerProvider) {
-		console.log(this.getPresentDate('ddd MMM DD  h:mma'));
+		let date = this.getPresentDate('ddd MMM DD  h:mma');
+		console.log("First",date);
+		let newYork = moment.tz(date, "EST");
+		console.log("second", newYork.format('ddd MMM DD  h:mma'));
+		var losAngeles = moment.tz("2014-06-01 12:00", "America/New_York");
+		console.log(losAngeles.format());
 	}
 
-
-
-
 	getPresentDate(dateFormat){
-		return moment().format(dateFormat);
+		return moment().format();
 	}
 
 	ngOnInit() {

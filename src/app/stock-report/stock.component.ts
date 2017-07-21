b3cdcf18615650c734c62375aa374c21fd32b07f
ali yeysides <ali.yeysides@gmail.com>
2017-07-21 11:29:22 -0400

    adds dynamic stock summary src

--- src/app/stock-report/stock.component.ts
+++ src/app/stock-report/stock.component.ts
@@ -1,20 +1,40 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {DomSanitizer, SafeUrl} from '@angular/platform-browser';
+import {environment} from '../../environments/environment';
 
 @Component({
-	selector: 'mid-tier-stock',
-	templateUrl: './stock.component.html',
-	styleUrls: ['./stock.component.scss'],
-	encapsulation: ViewEncapsulation.None
+  selector: 'mid-tier-stock',
+  templateUrl: './stock.component.html',
+  styleUrls: ['./stock.component.scss'],
+  encapsulation: ViewEncapsulation.None
 
 })
 export class StockComponent implements OnInit {
 
-	constructor() {
+  public symbol: string;
+  public src: string;
+  public sanitizedSrc: SafeUrl;
 
-	}
+  apiHostName = environment.envProtocol + '://' + environment.envHostName;
 
-	ngOnInit() {
+  constructor(private route: ActivatedRoute,
+              private sanitizer: DomSanitizer) {
+  }
 
-	}
+  ngOnInit() {
+    this.route.params
+      .subscribe(params => {
+          console.log('activated route', params);
+          if (params.symbol) {
+            this.symbol = params.symbol;
+          } else {
+            this.symbol = 'AAPL';
+          }
+        this.src = `https://dev.chaikinanalytics.com/CPTRestSecure/StockSummary/index.html?lang=English&uid=9582&environment=desktop&subEnvironment=chaikinAnalytics&version=1.3.2&symbol=${this.symbol}&userType=CAUser`;
+        this.sanitizedSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.src);
+        }
+      )
+  }
 
 }

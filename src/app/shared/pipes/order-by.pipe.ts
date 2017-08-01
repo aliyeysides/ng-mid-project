8bc4afcb079baa27bbb8bcd0c588e4a666aa0a3b
ali yeysides <ali.yeysides@gmail.com>
2017-08-01 14:03:08 -0400

    fix sort by marketCap

--- src/app/shared/pipes/order-by.pipe.ts
+++ src/app/shared/pipes/order-by.pipe.ts
@@ -6,13 +6,14 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class OrderByPipe implements PipeTransform {
 
   transform(array, orderBy, asc = true){
-    console.log('array', array, 'orderBy', orderBy);
+    console.log('array in', array, 'orderBy', orderBy);
 
     // if array has multidimensional objects, reassign object meta-info to top level
     if (array.length && array[0].hasOwnProperty('meta-info')) {
       for (let i=0;i<array.length;i++) {
         Object.assign(array[i], array[i]['meta-info'])
       }
+      console.log('array out', array);
     }
 
     if (!orderBy || orderBy.trim() == ""){

eda00efdf36fe4357c9b661372937f1ddd77665f
CHRISTIAN BELLUSO <christianbelluso@CHRISTIANs-MacBook-Pro.local>
2017-08-11 08:49:28 -0400

    discovery cleanup

--- src/assets/js/cmtScript.js
+++ src/assets/js/cmtScript.js
@@ -480,6 +480,35 @@ $(document).ready(function () {
     $(".sidebar__container").toggleClass("collapsed");
     $(".body").toggleClass("collapsed");
   });
+
+  
+
+    $(".discovery__tile").mouseover(function(){
+        $(this).addClass("selected");
+    });
+
+    $(".discovery__tile").mouseout(function(){
+        $(this).removeClass("selected");
+    });
+
+
+    $(".discovery-results__slider").mouseover(function(){
+        $(this).addClass("selected-slider");
+    });
+
+    $(".discovery-results__slider").mouseout(function(){
+        $(this).removeClass("selected-slider");
+    });
+
+
+
+
+    $(".cornerFlip").click(function(){
+        $(".discovery__tile.selected").addClass('flipped').mouseleave(function(){
+            $(this).removeClass('flipped');
+        });
+        return false;
+    });
 //
 //
 //   $(".headlines-expand").click(function () {

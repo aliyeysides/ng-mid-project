4c0c4c7ec500ecc97b47198db18323b7b5cacab7
ali yeysides <ali.yeysides@gmail.com>
2017-08-22 15:37:05 -0400

    moar refactor

--- src/app/shared/list-selection/list-selection.module.ts
+++ src/app/shared/list-selection/list-selection.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ListSelectionComponent} from './list-selection.component';
-
+import {TooltipModule} from 'ngx-bootstrap/tooltip';
 import {PipesModule} from '../pipes/pipes.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    TooltipModule,
     PipesModule
   ],
   exports: [

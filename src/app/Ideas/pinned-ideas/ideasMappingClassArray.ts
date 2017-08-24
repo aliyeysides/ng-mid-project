8c7f443b539e1a0c1c9fe3947895419b32612b03
C Belluso <cbelluso@Christians-iMac.local>
2017-08-24 10:59:15 -0400

    img list ts file and some css tweaks

--- src/app/Ideas/pinned-ideas/ideasMappingClassArray.ts
+++ src/app/Ideas/pinned-ideas/ideasMappingClassArray.ts
@@ -1,133 +1,233 @@
 export const mappingClassArray: any = {
     'Holding': {
-      'style': 'list__option--userlist list__option--holding',
+      'style': 'list__option--userlist',
       'imgName': 'img_list-holding.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Watching': {
-      'style': 'list__option--userlist list__option--watching',
+      'style': 'list__option--userlist',
       'imgName': 'img_list-watching.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Ideas for You': {
-      'style': 'list__option--userlist list__option--ideasforyou',
+      'style': 'list__option--userlist',
       'imgName': 'img_list-ideasforyou.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Bulls of the Week': {
-      'style': 'list__option--bulllist list__option--classicbulls',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-classicbulls.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Best Growth Stocks': {
-      'style': 'list__option--bulllist list__option--growthstock',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-growthstock.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Best of the Large Caps': {
-      'style': 'list__option--bulllist list__option--largecap',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-largecap.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Best of the NASDAQ': {
-      'style': 'list__option--bulllist list__option--bestofthenasdaq',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-bestofthenasdaq.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Best of the Small Caps': {
-      'style': 'list__option--bulllist list__option--smallcap',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-smallcap.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Best Under $10': {
-      'style': 'list__option--bulllist list__option--underten',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-underten.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Best Value Stocks': {
-      'style': 'list__option--bulllist list__option--bestvalue',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-bestvalue.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Buy the Dips': {
-      'style': 'list__option--bulllist list__option--buydips',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-selldips.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Insider Confidence': {
-      'style': 'list__option--bulllist list__option--insiderconfidence',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-insiderconfidence.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Money Makers': {
-      'style': 'list__option--bulllist list__option--moneymakers',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-moneymakers.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Relative Strength Champs': {
-      'style': 'list__option--bulllist list__option--relativestrength',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-relativestrength.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Money Flow Champs': {
-      'style': 'list__option--bulllist list__option--buyingstrength',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-buyingstrength.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Analyst Darlings': {
-      'style': 'list__option--bulllist list__option--analystdarlings',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-analystdarlings.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Power Gauge Rating Upgrades': {
-      'style': 'list__option--bulllist list__option--PGRupgrade',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-PGRupgrade.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Best of the Dow': {
-      'style': 'list__option--bulllist list__option--bestofthedow',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-bestofthedow.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Earnings Champs': {
-      'style': 'list__option--bulllist list__option--earningschamps',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-earningschamps.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Sell the Rallies': {
-      'style': 'list__option--bearlist list__option--sellrallies',
+      'style': 'list__option--bearlist',
       'imgName': 'img_list-buyrallies.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Bears of the Week': {
-      'style': 'list__option--bearlist list__option--classicbears',
+      'style': 'list__option--bearlist',
       'imgName': 'img_list-classicbears.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Power Gauge Rating Downgrades': {
-      'style': 'list__option--bearlist list__option--PGRdowngrade',
+      'style': 'list__option--bearlist',
       'imgName': 'img_list-PGRdowngrade.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Don\'t Fight the Shorts': {
-      'style': 'list__option--bearlist list__option--dontfighttheshorts',
+      'style': 'list__option--bearlist',
       'imgName': 'img_list-dontfighttheshorts.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Dogs of the Dow': {
-      'style': 'list__option--bearlist list__option--dogsofthedow',
+      'style': 'list__option--bearlist',
       'imgName': 'img_list-dogsofthedow.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Upcoming Earnings Bears': {
-      'style': 'list__option--bearlist list__option--earningsbears',
+      'style': 'list__option--bearlist',
       'imgName': 'img_list-earningsbears.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
     },
     'Upcoming Earnings Bulls': {
-      'style': 'list__option--bulllist list__option--earningsbulls',
+      'style': 'list__option--bulllist',
       'imgName': 'img_list-earningsbulls.svg',
       'description': 'This list contains companies who have exceeded earnings expectations on a quartly basis'
-    }
+    },
+
+
+
+
+
+    'Big Data': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-bigdata.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'China Shop': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-china.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'Cybersecurity': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-security.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+    
+    'Disruptors': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-disruptors.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'E-Payments': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-epayment.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+    
+    'Gold Standards': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-gold.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'Internet Innovators': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-internet.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'Social Butterflies': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-socialmedia.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'Video Games': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-videogames.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+    
+    'Fashion & Luxury': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-luxury.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'Defense Titans': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-defense.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+    
+    'Earth Friendly': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-ecofriendly.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'Upwardly Mobile': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-mobile.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'Sin City': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-sincity.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'Health & Fitness': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-fitness.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
+
+    'Cloud Computing': {
+      'style': 'list__option--themelist',
+      'imgName': 'img_list-cloud.svg',
+      'description': 'This theme list is all about this exact theme'
+    },
 
   };

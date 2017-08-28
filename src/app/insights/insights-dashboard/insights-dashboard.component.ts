e9e4f248f3cb68cdde7c74ba3575ceede1d68ba9
ali yeysides <ali.yeysides@gmail.com>
2017-08-28 14:31:04 -0400

    progress

--- src/app/insights/insights-dashboard/insights-dashboard.component.ts
+++ src/app/insights/insights-dashboard/insights-dashboard.component.ts
@@ -17,6 +17,7 @@ export class InsightsDashboardComponent implements OnInit {
   public earningsIdea: object;
   public bullsandBears: object;
   public stockoftheWeek: object;
+  public marketInsights: object[];
   public expertEducationPosts: object[];
 
   constructor(private modalService: BsModalService,
@@ -28,6 +29,7 @@ export class InsightsDashboardComponent implements OnInit {
     this.sharedService.getWordPressJson('16').subscribe(res => this.earningsIdea = res['0']['16'][0]);
     this.sharedService.getWordPressJson('13').subscribe(res => this.bullsandBears = res['0']['13'][0]);
     this.sharedService.getWordPressJson('22').subscribe(res => this.stockoftheWeek = res['0']['22'][0]);
+    this.sharedService.getWordPressJson('47').subscribe(res => this.marketInsights = res['0']['47']);
     this.sharedService.getWordPressJson('46').subscribe(res => {
       this.expertEducationPosts = res['0']['46'];
       this.assignAuthorProp(this.expertEducationPosts);
@@ -61,6 +63,12 @@ export class InsightsDashboardComponent implements OnInit {
     this.bsModalRef = this.modalService.show(InsightsModalComponent);
     this.bsModalRef.content.title = 'Stock of the Week';
     this.bsModalRef.content.body = this.getInsightPostBody(this.stockoftheWeek);
+  }
+
+  public openMarketInsight(post: object) {
+    this.bsModalRef = this.modalService.show(InsightsModalComponent);
+    this.bsModalRef.content.title = 'Market Insight';
+    this.bsModalRef.content.body = this.getInsightPostBody(post);
   }
 
   public openEducationPost(post: object) {
